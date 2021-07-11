9853a3ac6977b72ec56a62eb17acaee8881b8954
Richard Williamson <richard.williamson@fieldnation.com>
2021-07-11 11:18:33 -0500

    fix for deployment

--- index.js
+++ index.js
@@ -8,7 +8,32 @@ const User = require("./lib/model/user");
 const app = express();
 
 // the port we listen on
-const port = process.env.PORT || 8000;
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
 
 // allow us to parse body posts
 app.use(express.json());
